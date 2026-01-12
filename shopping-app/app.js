12bae8a51f254ee21a65189aa2cb256dfbb5a3fb
Thac Nguyen <minhthac1511@gmail.com>
2026-01-12 16:06:14 +1100

    update example files

--- shopping-app/app.js
+++ shopping-app/app.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
+
+app.use(express.static('public'));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Set the folder for EJS templates
+app.set('views', path.join(__dirname, 'views'));
 
 // Connect to MongoDB
 connectDB();
@@ -10,6 +18,9 @@ connectDB();
 app.use(express.json());
 
 // Your routes and middlewares here
+app.get('/homepage', (req, res) => {
+  res.render('homepage', { title: 'Home Page' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
