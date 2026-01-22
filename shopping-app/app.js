844cd8b68174736622607521d265940180ebb043
Thac Nguyen <minhthac1511@gmail.com>
2026-01-22 16:00:57 +1100

    update link in terminal

--- shopping-app/app.js
+++ shopping-app/app.js
@@ -19,8 +19,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // routes
-const router = require('./routes');
-app.use('/', router);
+const router = require("./routes");
+app.use("/", router);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+  console.log(`Link to homepage: http://localhost:${PORT}`);
+});
